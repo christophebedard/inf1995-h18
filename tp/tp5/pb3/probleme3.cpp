4f48c8b96667dbeb3fe4c7010737d678e713b785
Christophe <christophe.bourque-bedard@polymtl.ca>
2018-02-15 15:48:51 -0500

    Cleanup probleme 3 du TP5

--- tp/tp5/pb3/probleme3.cpp
+++ tp/tp5/pb3/probleme3.cpp
@@ -1,12 +1,10 @@
-/* Fichier : probleme2.cpp
+/* Fichier : probleme3.cpp
 * Auteur : Bourque Bédard Christophe  & Mohamed Saddik
-* Date : 01 février 2018
+* Date : 15 février 2018
 *
-* Le programme permet de faire varier les couleurs de la LED 
-* en fonction de la pression et du relachement du bouton poussoir.
+* Le programme utilise un PWM materiel pour faire tourner les 
+* roues ou faire allumer un DEL a intensite variable.
 */
-
-
 
 #define F_CPU 8000000
 #include <avr/io.h>
@@ -14,80 +12,64 @@
 #include <avr/interrupt.h>
 
 // Definition des constantes
-const uint8_t PIND_BOUTON = 0X04;
 const uint8_t SORTIE = 0xff;
 const uint8_t ENTREE = 0X00;
-const uint8_t ETEINT = 0x00;
-const uint8_t ROUGE = 0x02;
-const uint8_t VERT = 0x01;
-const uint8_t DELAI_1 = 10;
-
-
-
-volatile uint8_t minuterieExpiree=0;
-volatile uint8_t boutonPoussoir=0; 
-
 
 /********************************************************************
 * Cette fonction de de l'initialisation 
 *********************************************************************/
-void initialisation ( void ) {
+void initialisation() {
 	// de-activation des interruptions
 	cli ();
 
 	// configuration des ports
-	DDRB = SORTIE; // Port B en mode sortie.
 	DDRD = SORTIE; // Port D en mode entrée.
-	
-/*
-	// interruption externe sur INT0
-	EIMSK |= (1 << INT0) ;
 
-	// interruption lors d'un rising ou falling edge
-	EICRA |= (1 << ISC00) | (1 << ISC01);
-*/
 	// re-activation des interruptions
 	sei ();
 }
 
 /********************************************************************
-* L'initialisation du PWM dans le timer1 
+* Initialisation du PWM avec le timer1 
 *********************************************************************/
-void initPWM () {
-	// division d'horloge par 8 - implique une frequence de PWM fixe
+void initPWM() {
+	// compare output mode 10 (Clear OCnA/OCnB on Compare Match (Set output to low level))
+	// waveform generation mode 1 (PWM, Phase Correct, 8-bit)
 	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 <<WGM10);
-	TCCR1B |= (1 << CS11); // division par 8
+	// division d'horloge
+	TCCR1B |= (1 << CS11);
 	TCCR1C = 0;
 }
 
 /********************************************************************
-* L'ajustement du PWM dans le timer1 
+* Ajustement du ratio du PWM
+* 
+* \param ratio : ratio du PWM en pourcent [0, 100]
 *********************************************************************/
-void ajustementPWM ( int ratio  ) {
+void ajustementPWM(uint8_t ratio) {
 	// mise à un des sorties OC1A et OC1B sur comparaison
 	// réussie en mode PWM 8 bits, phase correcte
 	// et valeur de TOP fixe à 0xFF (mode #1 de la table 17-6
-	// page 177 +++++++++++++++++++++
-	OCR1B = ratio * 255 / 100  ;
-	OCR1A = ratio * 255 / 100  ;
-	
+	// page 177 de la description technique du ATmega324PA)
+	OCR1A = ratio * 255 / 100;
+	OCR1B = ratio * 255 / 100;
 }
 
 /*******************************************************************************
-* La fonction principale. 
+* Fonction principale. 
 *******************************************************************************/
 int main() {
-	initialisation ();
+	initialisation();
 	initPWM();
 	
-	ajustementPWM (0);
+	ajustementPWM(0);
 	_delay_ms(2000);
-	ajustementPWM (25);
+	ajustementPWM(25);
 	_delay_ms(2000);
-	ajustementPWM (50);
+	ajustementPWM(50);
 	_delay_ms(2000);
-	ajustementPWM (75);
+	ajustementPWM(75);
 	_delay_ms(2000);
-	ajustementPWM (100);
+	ajustementPWM(100);
 	return 0;
 }
