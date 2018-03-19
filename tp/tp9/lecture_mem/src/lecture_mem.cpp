54fde4c4325e14ec2f598a4f7993bfad220f2489
christophebedard <chrisvolkoff@gmail.com>
2018-03-19 10:20:22 -0400

    Ajout d'un programme de lecture de la memoire pour tester

---
+++ tp/tp9/lecture_mem/src/lecture_mem.cpp
@@ -0,0 +1,44 @@
+/**
+ * \file lecture_mem.h
+ * \brief programme qui lit les octets en memoire et les transmet par UART pour verification
+ * \author 
+ */
+
+#include "memoire_24.h"
+#include "delai.h"
+#include "uart.h"
+#include "debug.h"
+
+
+int main()
+{
+    // sequence initiale
+    PORTA = ROUGE;
+    waitForMs(250);
+    PORTA = VERT;
+    waitForMs(250);
+    PORTA = ROUGE;
+    waitForMs(250);
+    PORTA = VERT;
+    waitForMs(250);
+    PORTA = ETEINT;
+
+    // transmission par UART et lecture dans memoire
+    UART::init();
+    Memoire24CXXX mem;
+    uint16_t adr = 0x00;
+
+    uint8_t buf[1];
+
+    // lecture et transmission
+    while(true)
+    {
+        mem.lecture(adr++, buf);
+        Debug::out(*buf, 16);
+    }
+
+    // fin
+    PORTA = VERT;
+
+    return 0;
+}
