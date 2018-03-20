b647c9551ddef83d5dfc9ce911694ce5cbe9326f
christophebedard <chrisvolkoff@gmail.com>
2018-03-20 18:23:44 -0400

    Ajout de transmissions debug pour test_buzzer

--- tp/tp9/test_buzzer/src/test_buzzer.cpp
+++ tp/tp9/test_buzzer/src/test_buzzer.cpp
@@ -6,21 +6,32 @@
 
 #include "delai.h"
 #include "buzzer.h"
+#include "debug.h"
 
 
 int main()
 {
+    UART::init(); // pour debug
+
     Buzzer::init();
 
+    Debug::out("debut\n");
+
     Buzzer::play(45);
+    Debug::out("45\n");
     waitForMs(1000);
     Buzzer::play(60);
+    Debug::out("60\n");
     waitForMs(1000);
     Buzzer::play(70);
+    Debug::out("70\n");
     waitForMs(1000);
     Buzzer::play(80);
+    Debug::out("80\n");
     waitForMs(1000);
+
     Buzzer::stop();
+    Debug::out("stop\n");
 
     while(true);
 
