5e26c23942fc2281cda07389d3fb036f4ab42c5d
Mohamed Saddik <mosadd@l3818-09.info.polymtl.ca>
2018-03-20 11:20:59 -0400

    le bytecode à tester

--- tp/tp9/tp9_bytecode.cpp
+++ tp/tp9/tp9_bytecode.cpp
@@ -1,6 +1,49 @@
 
 
 
-DBT; //Debut programme
+dbt; //Debut programme
+sgo 45;
+att 20;
+sgo 60;
+att 40;
+sgo 80;
+att 20;
+dal 32;
+sar;
+mar; 
+mav 255;
+mav 128;
+trd;
+det 32;
+mav 255;
+att 60;
+dbc 1;
+dal 32;
+att 25;
+del 32;
+trg;
+att 50;
+dal 32;
+att 40;
+det 32; 
+fdc;
+trg;
+att 40;
+trg;
+att 40;
+trg;
+att 40;
+trg;
+att 40;
+trg;
+att 40;
+trg;
+att 40;
+trg;
+att 40;
+mre 255;
+att 120;
+mar;
+fin; //Fin programme
 
-FIN; //Fin programme
+
