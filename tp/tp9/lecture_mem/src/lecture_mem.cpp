50ba8fe81946fc3ccfed761e2f5064c3c791a008
christophebedard <chrisvolkoff@gmail.com>
2018-03-20 18:22:49 -0400

    Meilleur formatting de transmission pour lecture_mem

--- tp/tp9/lecture_mem/src/lecture_mem.cpp
+++ tp/tp9/lecture_mem/src/lecture_mem.cpp
@@ -27,16 +27,58 @@ int main()
     // transmission par UART et lecture dans memoire
     UART::init();
     Memoire24CXXX mem;
-    uint16_t adr = 0x00;
 
+    uint16_t adr = 0x0;
     uint8_t buf[1];
 
-    // lecture et transmission
-    while(true)
+    // lecture des octets de taille
+    uint8_t tailleH = 0;
+    uint8_t tailleL = 0;
+    uint16_t taille = 0;
+    mem.lecture(adr++, &tailleH);
+    waitForMs(5);
+    mem.lecture(adr++, &tailleL);
+    waitForMs(5);
+    taille = (tailleH << 8) | (tailleL);
+
+    // lecture et transmission des octets
+    // (le reset de l'adresse mene a la lecture des 2 premiers octets une deuxieme
+    //   fois, mais cela permet de simplifier la boucle de transmission des octets)
+    adr = 0x0;
+    while(adr < taille)
     {
-        mem.lecture(adr++, buf);
-        Debug::out(*buf, 16);
+        // affichage de l'adresse
+        if ((adr % 2) == 0)
+        {
+            Debug::out(adr, 16);
+            Debug::out(" : ");
+        }
+
+        // lecture de l'octet
+        mem.lecture(adr, buf);
+
+        // transmission de l'octet
+        Debug::out(buf[0], 16);
+
+        // separateur
+        if ((adr % 2) == 0)
+        {
+            Debug::out(" ");
+        }
+        else
+        {
+            Debug::out("\n");
+        }
+
+        adr++;
+        waitForMs(5);
     }
+
+    // transmission du nombre d'octets transmis (assez verbeux)
+    Debug::out("adr fin = ");
+    Debug::out(adr, 16);
+    Debug::out(" / ");
+    Debug::out(adr);
 
     // fin
     PORTA = VERT;
