a71677ba0a93b77ecaf94d0164ebae103e566085
christophebedard <chrisvolkoff@gmail.com>
2018-03-26 17:22:47 -0400

    Ajout des informations sur l'equipe

--- projet/lib/include/defines.h
+++ projet/lib/include/defines.h
@@ -1,6 +1,12 @@
 #ifndef LIB_DEFINES_H
 #define LIB_DEFINES_H
 
+/**
+ * \file defines.h
+ * \brief declaration de divers types et constantes globales
+ * \author 
+ */
+
 #ifndef F_CPU
 #define F_CPU 8000000
 #endif
@@ -14,6 +20,12 @@ const uint8_t ENTREE = 0X00;
 const uint8_t ETEINT = 0x00;
 const uint8_t ROUGE = 0x02;
 const uint8_t VERT = 0x01;
+
+// Informations sur l'equipe
+const char INFO_NOM_ROBOT[] = "Thinky Winky";
+const char INFO_EQUIPE[] = "68116";
+const char INFO_GROUPE[] = "03";
+const char INFO_SESSION[] = "18-1";
 
 
 #endif // LIB_DEFINES_H
