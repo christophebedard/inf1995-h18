e48205ca79246b37f095179130e944e12768c64a
simord <simon.moreau@polymtl.ca>
2018-03-27 10:55:05 -0400

    Reset

--- projet/Diagnostic/Diagnostic/Diagnostic.cpp
+++ projet/Diagnostic/Diagnostic/Diagnostic.cpp
@@ -10,5 +10,45 @@ int main()
     DDRD = SORTIE:
     DDRB = SORTIE;
     DDRA = SORTIE;
-    return 0;
+    UART::init();
+    uint8_t instruction[1] = UART::reception()
+    for(;;){
+        switch (){
+        
+            case 0xf0:
+                
+            break;
+            
+            case 0xf1:
+                
+            break;
+            
+            case 0xf2:
+                
+            break;
+            
+            case 0xf3:
+                
+            break;
+                
+            case 0xf4:
+                
+            break;
+            
+            case 0xf5:
+                
+            break;
+            
+            case 0xf6:
+                
+            break;
+                
+            case 0xf7:
+                
+            break;
+            
+            default:
+            break;}
+    }
+   return 0;
 }
