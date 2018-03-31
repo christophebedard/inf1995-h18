1d1c038777c48b7f132116af72cc7155baa4a817
christophebedard <chrisvolkoff@gmail.com>
2018-03-31 16:11:44 -0400

    Refactor script python pour export donnees wav

--- projet/test_wav/src/test_wav.cpp
+++ projet/test_wav/src/test_wav.cpp
@@ -7,55 +7,79 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
-#include "samples.h"
+#include "wav_data.h"
 
-
+#define SAMPLE_COUNT_SCALE 2
 volatile uint16_t sample;
-int sample_count;
+volatile int sample_count;
+bool loop = false;
+volatile bool lecture;
 
 
 void init(void)
 {
     cli();
 
-    /* use OC1A pin as output */
+    // pin OC1A en sortie pour le buzzer
     DDRD = _BV(PD5);
 
-    /* 
-    * clear OC1A on compare match 
-    * set OC1A at BOTTOM, non-inverting mode
-    * Fast PWM, 8bit
-    */
+    // clear OC1A on compare match 
+    // set OC1A at BOTTOM, non-inverting mode
+    // fast PWM, 8bit
     TCCR1A = _BV(COM1A1) | _BV(WGM10);
     
-    /* 
-    * Fast PWM, 8bit
-    * Prescaler: clk/1 = 8MHz
-    * PWM frequency = 8MHz / (255 + 1) = 31.25kHz
-    */
+    // fast PWM, 8bit
+    // prescaler : clk/1 = 8 MHz
+    // PWM frequency = 8 MHz / (255 + 1) = 31.25 kHz
     TCCR1B = _BV(WGM12) | _BV(CS10);
     
-    /* set initial duty cycle to zero */
+    // reset
     OCR1A = 0;
     
-    /* Setup Timer0 */  
+    // timer0 pour la mise a jour des valeurs  
     TCCR0A |= (1 << CS00);
     TCCR0B |= (1 << CS00);
     TCNT0 = 0;
     TIMSK0 |= (1 << TOIE0);
-    sample_count = 4;
+
+    // initialisation du compteur de scale
+    sample_count = SAMPLE_COUNT_SCALE;
+    // initialisation de la lecture
+    lecture = true;
 
     sei();
 }
 
 ISR(TIMER0_OVF_vect)
 {
-    sample_count--;
-    if (sample_count == 0) 
+    if(lecture)
     {
-        sample_count = 4;           
-        OCR1A = pgm_read_byte(&pcm_samples[sample++]);
-        if(sample>=pcm_length)sample=0;
+        sample_count--;
+        
+        if(sample_count == 0) 
+        {
+            // reset le compteur de scale
+            sample_count = SAMPLE_COUNT_SCALE;
+            
+            // update la valeur
+            OCR1A = pgm_read_byte(&pcm_samples[sample++]);
+            
+            // verification fin des donnees
+            if(sample >= pcm_length)
+            {
+                // si loop fichier
+                if(loop)
+                {
+                    // recommence
+                    sample = 0;
+                }
+                else
+                {
+                    // sinon, arret
+                    lecture = false;
+                }
+            }
+        }
     }
 } 
 
--- util/python/wav_to_data.py
+++ util/python/wav_to_data.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-
+Lecture d'un fichier .wav et formatage en 
+fichier .h pour lecture avec le buzzer du robot
 @author: Christophe Bourque Bedard
 @date: 2018-03-29
+
+Utilisation : python wav_to_data.py input.wav [output.h]
 """
 from __future__ import print_function # pour avoir print() de python 3
 import sys
 import itertools
 import wave
+import librosa
+import soundfile as sf
 
 
+# taux d'echantillonnage voulu [Hz]
+TAUX_ECHANTILLONNAGE = 8000
 # nombre de resultats par ligne
 NB_VAL_PAR_LIGNE = 5
+# nom du fichier header des donnees exportees
+NOM_FICHIER_DATA_DEFAULT = "wav_data.h"
 
 
-def wavToData(nom_fichier):
+def wav_to_data(nom_fichier):
     """
-
+    Lecture du fichier et recuperation des donnees
     :param nom_fichier: le nom du fichier .wav
+    :return: donnees
     """
     w = wave.open(nom_fichier, 'r')
-    
+
+    # verifie le sample rate
+    if w.getframerate() != TAUX_ECHANTILLONNAGE:
+        print("attention : taux d'echantillonnage !=", TAUX_ECHANTILLONNAGE, "Hz\n")
+        """
+        print("[re-echantillonnage a", TAUX_ECHANTILLONNAGE, "Hz]\n")
+        w.close()
+        nom_fichier_resampled = 's_' + nom_fichier
+        # re-load et re-sample
+        data, sample_rate = librosa.load(nom_fichier, sr=TAUX_ECHANTILLONNAGE, mono=True, res_type='scipy')
+        # ecrit le fichier en WAV PCM unsigned 8 bit
+        sf.write(nom_fichier_resampled, data, sample_rate, subtype='PCM_U8')
+        # re-load le fichier pour continuer
+        w = wave.open(nom_fichier_resampled, 'r')
+        """
+
     print("infos\n")
     print("nb channels  :", w.getnchannels())
     print("sample width :", w.getsampwidth())
@@ -32,6 +57,7 @@ def wavToData(nom_fichier):
     # get frames
     nb_frames = w.getnframes()
     frames = w.readframes(nb_frames)
+    w.close()
 
     # conversion des frames en entiers
     frames_int = [ord(f) for f in frames]
@@ -39,9 +65,15 @@ def wavToData(nom_fichier):
     return frames_int
 
 
-def formatWavData(data, file):
-    print("\necriture dans le fichier :", file)
-    with open(file, 'w') as f:
+def export_wav_data(data, nom_fichier_export):
+    """
+    Formatage des donnees pour ecriture dans un fichier
+    :param data: donnees
+    :param nom_fichier_export: nom du fichier d'exportation
+    :
+    """
+    print("\necriture dans le fichier :", nom_fichier_export)
+    with open(nom_fichier_export, 'w') as f:
         print('const long pcm_length = ', len(data), ';', sep='', file=f)
         print(file=f)
         print('const unsigned char pcm_samples[] PROGMEM = {', file=f)
@@ -68,19 +100,19 @@ def main():
     # verifie le nombre d'arguments
     narg = len(sys.argv)
     if narg <= 1:
-        print("erreur : specifiez au moins le nom du fichier .wav avec l'extension")
+        print("erreur : specifiez au moins le nom du fichier avec l'extension .wav")
     elif narg >= 2:
         # condition normale
         # parse les arguments des noms de fichier
         nom_fichier_wav = sys.argv[1]
         if narg == 2:
-            nom_fichier_data = "samples.h"
+            nom_fichier_data = NOM_FICHIER_DATA_DEFAULT
         else:
             nom_fichier_data = sys.argv[2]
 
         # execute
-        data = wavToData(nom_fichier_wav)
-        formatWavData(data, nom_fichier_data)
+        data = wav_to_data(nom_fichier_wav)
+        export_wav_data(data, nom_fichier_data)
 
 
 if __name__ == "__main__":
